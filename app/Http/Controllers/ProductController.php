d7deb4e2363734f0e289c039fa98c6a4588e99b0
Kenneth Obeng <obengkenneth3@gmail.com>
2023-07-16 10:13:06 +0000

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product; 
+
+class ProductController extends Controller
+{
+    public function index(){
+        return view('products.index');
+    }
+
+    public function create(){
+        return view('products.create');
+    }
+
+    public function store(Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,4',
+            'description' => 'required'
+        ]);
+
+        $newProduct = Product::create($data);
+
+        return redirect(route('product.index'));
+    }
+}
