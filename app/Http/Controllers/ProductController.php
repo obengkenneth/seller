a4a40a9dc1eaceab92cefa7f236964888d2de035
Kenneth Obeng <obengkenneth3@gmail.com>
2023-07-16 13:43:18 +0000

    Added edit and delete functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,8 @@ use App\Models\Product;
 class ProductController extends Controller
 {
     public function index(){
-        return view('products.index');
+        $products = Product::all();
+        return view('products.index', ['products' => $products]);
     }
 
     public function create(){
@@ -26,5 +27,27 @@ class ProductController extends Controller
         $newProduct = Product::create($data);
 
         return redirect(route('product.index'));
+    }
+
+    public function edit(Product $product){
+        return view('products.edit', ['product' => $product]);
+    }
+
+    public function update(Product $product, Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,4',
+            'description' => 'required'
+        ]);
+
+        $product->update($data);
+
+        return redirect(route('product.index'))->with('success', 'Product updated successfully');
+    }
+    
+    public function delete(Product $product){
+        $product->delete();
+        return redirect(route('product.index'))->with('success', 'Product deleted successfully');
     }
 }
